ac004429d118415d9997fbc6b2ecdf87c0f6b5d7
hanbincho <hanbincho4261@gmail.com>
2023-07-05 15:24:08 -0700

    Ensure ground connection is made for all stimulation modes

--- gRPC/Client Examples/RealtimeGraphing/BICManager.cs
+++ gRPC/Client Examples/RealtimeGraphing/BICManager.cs
@@ -192,7 +192,7 @@ namespace RealtimeGraphing
                         StimulationFunctionDefinition pulseFunction0 = new StimulationFunctionDefinition()
                         {
                             FunctionName = "openLoopPulse",
-                            StimPulse = new stimPulseFunction() { Amplitude = { stimAmplitude, 0, 0, 0 }, DZ0Duration = 10, DZ1Duration = interPulseInterval, PulseWidth = stimDuration, PulseRepetitions = 1, SourceElectrodes = { stimChannel }, SinkElectrodes = { returnChannel }, UseGround = false, BurstRepetitions = 1 }
+                            StimPulse = new stimPulseFunction() { Amplitude = { stimAmplitude, 0, 0, 0 }, DZ0Duration = 10, DZ1Duration = interPulseInterval, PulseWidth = stimDuration, PulseRepetitions = 1, SourceElectrodes = { stimChannel }, SinkElectrodes = { returnChannel }, UseGround = true, BurstRepetitions = 1 }
 
                         };
                         aNewWaveformRequest.Functions.Add(pulseFunction0);
@@ -225,7 +225,7 @@ namespace RealtimeGraphing
                         StimulationFunctionDefinition pulseFunction0 = new StimulationFunctionDefinition()
                         {
                             FunctionName = "openLoopPulse",
-                            StimPulse = new stimPulseFunction() { Amplitude = { stimAmplitude, 0, 0, 0 }, DZ0Duration = 10, DZ1Duration = 10, PulseWidth = stimDuration, PulseRepetitions = 1, SourceElectrodes = { stimChannel }, SinkElectrodes = { returnChannel }, UseGround = false, BurstRepetitions = 1 }
+                            StimPulse = new stimPulseFunction() { Amplitude = { stimAmplitude, 0, 0, 0 }, DZ0Duration = 10, DZ1Duration = 10, PulseWidth = stimDuration, PulseRepetitions = 1, SourceElectrodes = { stimChannel }, SinkElectrodes = { returnChannel }, UseGround = true, BurstRepetitions = 1 }
 
                         };
                         aNewWaveformRequest.Functions.Add(pulseFunction0);
